946ded32cac5097761c85f9dc67265bb64710815
RohanMehta45 <rohanmehta0045@gmail.com>
2022-10-26 20:08:37 +0530

    weather button added

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ function changeMode() {
     mybody.classList.toggle('darkmode');
 
 }
+
+
+let x = document.getElementById('out');
+let y = document.getElementById('weatherOut');
+
+function geolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+        x.innerText = "Geo Not Supported"
+    }
+}
+
+function showPosition(data) {
+    console.log(data)
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude;
+    let y = document.getElementById('weatherOut');
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+        //api calling
+    fetch(url, { method: 'GET' })
+        // return promise
+        .then((res) => res.json())
+        // resolve promise
+        .then((data) => {
+            console.log(data)
+            let cityName = data.city.name;
+            let temp = data.list[0].temp.day;
+            y.innerText = `City : ${cityName} \n Temperature : ${temp} °C`
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
